f742613e0e5ee0e4fdcba7eddc66f91b313f33ad
roach <tmdgusya@naver.com>
2021-08-09 19:47:26 +0900

    dfs

---
+++ src/dfs/solution.py
@@ -0,0 +1,18 @@
+import unittest
+
+def dfs(graph, v, visited):
+
+    visited[v] = True
+    print(v, end='->')
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+class TestClass(unittest.TestCase): 
+
+    def test_case_one(self):
+        graph = [[], [2,3,8], [1,7], [1,4,5], [3,5], [3,4], [7], [2,6,8], [1, 7]]
+        visited = [False] * 9
+        dfs(graph, 1, visited)
