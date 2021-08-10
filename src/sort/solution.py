19b6970ee64e08b7928019e6576998c87148a94e
roach <tmdgusya@naver.com>
2021-08-10 10:29:47 +0900

    feat: selection sort

--- src/sort/solution.py
+++ src/sort/solution.py
@@ -1,6 +1,6 @@
 import unittest
 
-
+# O(n) TimeComplexity
 def selection_sort(arr: list):
 
     for i in range(len(arr)):
@@ -12,8 +12,23 @@ def selection_sort(arr: list):
     
     return arr
 
+# O(n) TimeComplexity
+def insertion_sort(arr: list):
+
+    for i in range(1, len(arr)):
+        for j in range(i, 0, -1):
+            if arr[j] < arr[j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+            else:
+                break
+    return arr
+
 class TestClass(unittest.TestCase):
 
     def test_one(self):
         arr = [1,5,3,8,4,9]
         self.assertEqual(selection_sort(arr), [1,3,4,5,8,9])
+
+    def test_insertion_sort(self):
+        arr = [1,5,3,8,4,9]
+        self.assertEqual(insertion_sort(arr), [1,3,4,5,8,9])
